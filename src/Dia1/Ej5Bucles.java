eb4394540eb61a0976852368150d9d50f8eba143
Jaime Martin <jmartingarciacuerva@educa.madrid.org>
2026-05-11 12:16:07 +0200

    Ej5Bucles

--- src/Dia1/Ej5Bucles.java
+++ src/Dia1/Ej5Bucles.java
@@ -10,8 +10,11 @@ public class Ej5Bucles {
     public static void main(String[] args) {
 
         Scanner teclado = new Scanner(System.in);
+        // FOR
         int totalHoras=0;
         int horasDia=0;
+
+        System.out.println("**************FOR**************");
 
         for (int i = 0; i < 5; i++) {
             System.out.println("¿Cuantas horas has trabajado hoy?");
@@ -23,6 +26,40 @@ public class Ej5Bucles {
         System.out.println("Horas totales "+totalHoras);
         System.out.println("Sueldo total "+sueldoTotal);
 
+        //WHILE
+        System.out.println("**************WHILE**************");
+        totalHoras=0;
+        horasDia=0;
+        int i=0;
+
+        while (i<5){
+            System.out.println("¿Cuantas horas has trabajado hoy?");
+            horasDia = teclado.nextInt();
+            totalHoras=totalHoras+horasDia;
+            i++;
+        }
+
+        sueldoTotal= totalHoras*15;
+        System.out.println("Horas totales "+totalHoras);
+        System.out.println("Sueldo total "+sueldoTotal);
+
+        // DO-WHILE
+        System.out.println("**************DO-WHILE**************");
+
+        totalHoras=0;
+        horasDia=0;
+        i=0;
+
+        do{
+            System.out.println("¿Cuantas horas has trabajado hoy?");
+            horasDia = teclado.nextInt();
+            totalHoras=totalHoras+horasDia;
+            i++;
+        }while (i<5);
+
+        sueldoTotal= totalHoras*15;
+        System.out.println("Horas totales "+totalHoras);
+        System.out.println("Sueldo total "+sueldoTotal);
 
 
     }
