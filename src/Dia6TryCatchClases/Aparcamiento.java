539c16cd62fd28feb9354dd81500eb9377b74b26
Jaime Martin <jmartingarciacuerva@educa.madrid.org>
2026-05-19 10:04:19 +0200

    EjErrores

--- src/Dia6TryCatchClases/Aparcamiento.java
+++ src/Dia6TryCatchClases/Aparcamiento.java
@@ -2,6 +2,8 @@ package Dia6TryCatchClases;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Objects;
 
 public class Aparcamiento {
 
@@ -9,12 +11,13 @@ public class Aparcamiento {
     private LocalDateTime fechaHoraEntrada;
     private LocalDateTime fechaHoraSalida;
     private Double importe;
-    private static Double precioMinuto;
+    private static Double precioMinuto=0.05;
      static final DateTimeFormatter FORMATO = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
 
     public Aparcamiento(String matricula, LocalDateTime fechaHoraEntrada) {
         this.matricula = matricula;
+        //Si la fecha de entrada es posterior a la de hoy, lanza un error
         if (fechaHoraEntrada.isAfter(LocalDateTime.now())) {
             throw new FechaAparcamientoInvalidaException("Estas aparcando en un dia del futuro. No se puede");
         } else {
@@ -36,5 +39,29 @@ public class Aparcamiento {
                     ", fechaHoraSalida=" + fechaHoraSalida.format(FORMATO) +
                     ", importe=" + importe;
         }
+    }
+
+    public void saleCoche(LocalDateTime fechaHoraSalida){
+        this.fechaHoraSalida=fechaHoraSalida;
+        setImporte();//Calcula el importe, ahora que tenemos la fechahora de salida
+    }
+
+    private void setImporte() {
+        int minutosAparcado=(int)(fechaHoraEntrada.until(fechaHoraSalida, ChronoUnit.MINUTES));
+        this.importe= minutosAparcado*precioMinuto;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Aparcamiento that = (Aparcamiento) o;
+        return Objects.equals(matricula, that.matricula) && Objects.equals(fechaHoraEntrada, that.fechaHoraEntrada);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(matricula, fechaHoraEntrada);
     }
 }
--- src/Dia6TryCatchClases/AparcamientoMain.java
+++ src/Dia6TryCatchClases/AparcamientoMain.java
@@ -5,12 +5,28 @@ import java.time.LocalDateTime;
 public class AparcamientoMain {
     public static void main(String[] args) {
         Aparcamiento aparcamiento1=null;
+
+        //Creamos un aparcamiento.
+        //Como la fecha de entrada es posterior a hoy, recibiremos un error del constructor
         try {
             aparcamiento1 = new Aparcamiento("4444EEE", LocalDateTime.now().plusDays(1));
         }catch (FechaAparcamientoInvalidaException error){
             System.out.println(error.getMessage());
         }
         System.out.println(aparcamiento1);
+
+
+        //Creamos un aparcamiento. Con la fecha de entrada de hace 10 minutos
+        try {
+            aparcamiento1 = new Aparcamiento("4444EEE", LocalDateTime.now().minusMinutes(10));
+        }catch (FechaAparcamientoInvalidaException error){
+            System.out.println(error.getMessage());
+        }
+        System.out.println(aparcamiento1);
+        //Llamamos a saleCoche, con la fecha hora actual
+        aparcamiento1.saleCoche(LocalDateTime.now());
+        System.out.println(aparcamiento1);//Mostramos los datos, después de salir
+
 
 
     }
